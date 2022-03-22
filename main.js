64726d2ba3c6c11c6de697e50941df0c767d5f88
Leon Hochwimmer <leon.hochwimmer@coop.ch>
2022-03-22 10:01:18 +0100

    minor changes

--- main.js
+++ main.js
@@ -1,26 +1,25 @@
 const navSlide = () => {
-    const burger = document.querySelector('.burger');
-    const nav = document.querySelector('.nav-links');
-    const navLinks = document.querySelectorAll('.nav-links li');
+  const burger = document.querySelector(".burger");
+  const nav = document.querySelector(".nav-links");
+  const navLinks = document.querySelectorAll(".nav-links li");
 
-    
-    burger.addEventListener('click',() => {
-        //Toggle Nav
-        nav.classList.toggle('nav-active');
+  burger.addEventListener("click", () => {
+    //Toggle Nav
+    nav.classList.toggle("nav-active");
 
-        //Animate Links
-        navLinks.forEach((link, index) => {
-            if (link.style.animation) {
-                link.style.animation = ''
-            } else {
-                link.style.animation = `navLinkFade 250ms ease forwards ${index / 7 + 0.25}s`;
-            }
-        });
-        //Burger Animation
-        burger.classList.toggle('toggle');
+    //Animate Links
+    navLinks.forEach((link, index) => {
+      if (link.style.animation) {
+        link.style.animation = "";
+      } else {
+        link.style.animation = `navLinkFade 250ms ease forwards ${
+          index / 7 + 0.25
+        }s`;
+      }
     });
-}
-    
-
+    //Burger Animation
+    burger.classList.toggle("toggle");
+  });
+};
 
 navSlide();
